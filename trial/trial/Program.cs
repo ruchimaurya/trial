cbd8d75cf1ce59c5acbaa09982c7519fc5523198
Ruchi Maurya <ruchimaurya@promactinfo.com>
2018-06-18 17:56:17 +0530

    upload

---
+++ trial/trial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "This is a string.";
+            String sub1 = "this";
+            Console.WriteLine("Does '{0}' contain '{1}'?", s, sub1);
+            StringComparison comp = StringComparison.Ordinal;
+            Console.WriteLine("   {0:G}: {1}", comp, s.Contains(sub1, comp));
+
+            comp = StringComparison.OrdinalIgnoreCase;
+            Console.WriteLine("   {0:G}: {1}", comp, s.Contains(sub1, comp));
+        }
+    }
+}
